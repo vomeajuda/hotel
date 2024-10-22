453df81a8365afbdacc434db3872e5526c04bcd1
vomeajuda <leogugo9@hotmail.com>
2024-10-22 14:08:56 -0300

    novo banco

--- bd/hotel_ds.sql
+++ bd/hotel_ds.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2024 at 09:49 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Tempo de geração: 22-Out-2024 às 19:04
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `hotel_ds`
+-- Banco de dados: `hotel_ds`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `quartos`
+-- Estrutura da tabela `quartos`
 --
 
 CREATE TABLE `quartos` (
@@ -34,23 +34,36 @@ CREATE TABLE `quartos` (
   `microondas` tinyint(1) DEFAULT NULL,
   `frigobar` tinyint(1) DEFAULT NULL,
   `tv` tinyint(1) DEFAULT NULL,
-  `cpf` varchar(15) NOT NULL,
+  `cpf` varchar(15) DEFAULT NULL,
   `ocupado` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `quartos`
+-- Extraindo dados da tabela `quartos`
 --
 
 INSERT INTO `quartos` (`N_Quarto`, `acomoda`, `varanda`, `microondas`, `frigobar`, `tv`, `cpf`, `ocupado`) VALUES
-(102, 2, 0, 1, 1, 0, '98765432100', 1);
+(101, 2, 1, 1, 1, 1, NULL, 0),
+(102, 2, 0, 1, 1, 0, '98765432100', 0);
 
 --
--- Indexes for dumped tables
+-- Acionadores `quartos`
+--
+DELIMITER $$
+CREATE TRIGGER `update_cpf` BEFORE UPDATE ON `quartos` FOR EACH ROW BEGIN
+    IF NEW.ocupado = 0 THEN
+        SET NEW.cpf = null;
+    END IF;
+END
+$$
+DELIMITER ;
+
+--
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `quartos`
+-- Índices para tabela `quartos`
 --
 ALTER TABLE `quartos`
   ADD PRIMARY KEY (`N_Quarto`);
